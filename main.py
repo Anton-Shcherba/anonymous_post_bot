56eefb300d63503d048516f27487c05189bafc06
Anton-Shcherba <tony.shcherba@gmail.com>
2023-11-23 09:39:02 +0300

    refactor: put texts into a separate file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import config
+import config, text
 import asyncio
 from aiogram import Bot, Dispatcher, F, Router
 from aiogram.enums import ParseMode
@@ -13,7 +13,6 @@ from aiogram.types import (
     InlineKeyboardMarkup,
     InlineKeyboardButton,
     CallbackQuery,
-    User,
     ErrorEvent,
 )
 from typing import Optional
@@ -44,31 +43,30 @@ def answer_keyboard(
     to_whom_id: int, replay: bool = False, support: bool = False
 ) -> InlineKeyboardMarkup:
     anon_btn_data = CallbackFactory(action=Action.send_from_user, id=to_whom_id).pack()
-    anon_btn_text = "🔄 Отправить ещё" if replay else "🔄 Ответить"
+    anon_btn_text = text.send_more if replay else text.answer
     anon_btn = InlineKeyboardButton(text=anon_btn_text, callback_data=anon_btn_data)
     if not support:
         return InlineKeyboardMarkup(inline_keyboard=[[anon_btn]])
     supp_btn_data = CallbackFactory(action=Action.send_from_supp, id=to_whom_id).pack()
-    supp_btn_text = "⚠️ Ответить"
+    supp_btn_text = text.answer_as_supp
     supp_btn = InlineKeyboardButton(text=supp_btn_text, callback_data=supp_btn_data)
     return InlineKeyboardMarkup(inline_keyboard=[[anon_btn], [supp_btn]])
 
 
 def cancel_keyboard() -> ReplyKeyboardMarkup:
-    button = KeyboardButton(text="♻️ Отмена")
+    button = KeyboardButton(text=text.cancel)
     return ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True)
 
 
 def profile_keyboard(user_id: int) -> InlineKeyboardMarkup:
-    button = InlineKeyboardButton(text="👤 Профиль", url=f"tg://user?id={user_id}")
+    button = InlineKeyboardButton(text=text.profile, url=f"tg://user?id={user_id}")
     return InlineKeyboardMarkup(inline_keyboard=[[button]])
 
 
 async def send_link(message: Message, state: FSMContext) -> None:
     await state.clear()
-    template = "🔗 Вот твоя личная ссылка, опубликуй её и получай анонимные сообщения"
-    link = f"<code>t.me/anonymous_post_bot?start={message.from_user.id}</code>"
-    await message.answer(template + "\n\n" + link, reply_markup=ReplyKeyboardRemove())
+    start_link_msg = text.start_msg_tmpl.format(id=message.from_user.id)
+    await message.answer(start_link_msg, reply_markup=ReplyKeyboardRemove())
 
 
 @form_router.message(CommandStart())
@@ -80,17 +78,11 @@ async def cmd_start_handler(
             await send_link(message, state)
         case str(message.from_user.id):
             await state.clear()
-            await message.answer(
-                f"⛔️ Извини, но ты не можешь отправить сообщение самому себе",
-                reply_markup=ReplyKeyboardRemove(),
-            )
+            await message.answer(text.to_myself_err, reply_markup=ReplyKeyboardRemove())
         case _:
             await state.set_state(Form.user_to_user)
             await state.update_data(user_to_user=command.args)
-            await message.answer(
-                f"💬 Отправь своё анонимное послание\n(текст, голосовое, фото, видео или др.)",
-                reply_markup=cancel_keyboard(),
-            )
+            await message.answer(text.send_msg, reply_markup=cancel_keyboard())
 
 
 @form_router.callback_query(CallbackFactory.filter(F.action == Action.send_from_user))
@@ -100,10 +92,7 @@ async def cb_from_user_handler(
     await callback.answer()
     await state.set_state(Form.user_to_user)
     await state.update_data(user_to_user=callback_data.id)
-    await callback.message.answer(
-        "💬 Отправь своё анонимное послание\n(текст, голосовое, фото, видео или др.)",
-        reply_markup=cancel_keyboard(),
-    )
+    await callback.message.answer(text.send_msg, reply_markup=cancel_keyboard())
 
 
 @form_router.callback_query(CallbackFactory.filter(F.action == Action.send_from_supp))
@@ -113,32 +102,20 @@ async def cb_from_support_handler(
     await callback.answer()
     await state.set_state(Form.support_to_user)
     await state.update_data(support_to_user=callback_data.id)
-    await callback.message.answer(
-        "⚠️ Сообщение будет отправлено от имени тех.поддержки",
-        reply_markup=cancel_keyboard(),
-    )
+    await callback.message.answer(text.send_from_supp, reply_markup=cancel_keyboard())
 
 
 @form_router.message(Command("cancel"))
-@form_router.message(F.text == "♻️ Отмена")
+@form_router.message(F.text == text.cancel)
 async def cancel_handler(message: Message, state: FSMContext) -> None:
     await state.clear()
-    await message.answer("♻️ Отмена отправки", reply_markup=ReplyKeyboardRemove())
+    await message.answer(text.cancel_msg, reply_markup=ReplyKeyboardRemove())
 
 
 @form_router.message(Command("support"))
 async def cmd_support_handler(message: Message, state: FSMContext) -> None:
     await state.set_state(Form.user_to_support)
-    await message.answer(
-        f"⚠️ Cледующее сообщение будет отправлено в чат технической пооддержки",
-        reply_markup=cancel_keyboard(),
-    )
-
-
-def html_id_link(user: User) -> str:
-    tg_link = f"tg://openmessage?user_id={user.id}"
-    html_link = f"<a href='{tg_link}'>{user.full_name}</a>"
-    return html_link
+    await message.answer(text.send_to_supp, reply_markup=cancel_keyboard())
 
 
 async def state_parse(state: FSMContext) -> tuple[str, bool, bool, bool]:
@@ -160,30 +137,19 @@ async def state_handler(message: Message, state: FSMContext) -> None:
     from_whom_id = message.from_user.id
     await state.clear()
 
-    new_msg_txt = "📨 Получено новое сообщение"
-    if for_spt:
-        new_msg_txt = "⚠️ Получено новое сообщение для тех.поддержки"
-    if from_spt:
-        new_msg_txt = "⚠️ Получено новое сообщение от тех.поддержки"
-    if for_admin:
-        new_msg_txt += f" от {html_id_link(message.from_user)}" if for_admin else ""
+    msg_txt = text.msg_recv_spt if for_spt or from_spt else text.msg_recv
+    msg_txt += text.id_link_tmpl.format(id=message.from_user.id) if for_admin else ""
     new_msg_kb = profile_keyboard(from_whom_id) if for_admin else None
-    await message.bot.send_message(to_whom_id, new_msg_txt, reply_markup=new_msg_kb)
+    await message.bot.send_message(to_whom_id, msg_txt, reply_markup=new_msg_kb)
 
-    await message.copy_to(
-        to_whom_id,
-        reply_markup=answer_keyboard(from_whom_id, support=for_admin)
-        if not from_spt
-        else None,
-    )
+    copy_msg_kb = None if from_spt else answer_keyboard(from_whom_id, support=for_admin)
+    await message.copy_to(to_whom_id, reply_markup=copy_msg_kb)
 
-    await message.answer("✅ Cообщение отправлено!", reply_markup=ReplyKeyboardRemove())
+    await message.answer(text.msg_sent, reply_markup=ReplyKeyboardRemove())
 
     if not (for_spt or from_spt):
-        await message.answer(
-            "Хотите отправить еще одно сообщение этому пользователю?",
-            reply_markup=answer_keyboard(to_whom_id, True),
-        )
+        one_more_kb = answer_keyboard(to_whom_id, True)
+        await message.answer(text.one_more_msg, reply_markup=one_more_kb)
 
 
 @form_router.message()
@@ -194,10 +160,7 @@ async def other_msg_handler(message: Message, state: FSMContext) -> None:
 @form_router.error(F.update.message.as_("message"))
 async def error_handler(event: ErrorEvent, message: Message, state: FSMContext) -> None:
     await state.clear()
-    await message.answer(
-        "⛔️ Упс, что-то пошло не так!",
-        reply_markup=ReplyKeyboardRemove(),
-    )
+    await message.answer(text.any_err, reply_markup=ReplyKeyboardRemove())
 
 
 async def main():
